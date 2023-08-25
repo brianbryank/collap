f94fc3ec5ad69df7b2271754d95ea154f152417a
brianbryank <brianbryan0125@gmail.com>
2023-08-25 18:16:48 +0300

    add briancomponent

--- collab/src/Brian.js
+++ collab/src/Brian.js
@@ -1,7 +1,18 @@
-function Brian(){
-    return <div>
-        <h1> Hello um a craft-master </h1>
+import React from 'react';
+import Brian from './Brian';
+import { Link } from "react-router-dom"
+
+function Brian() {
+  return (<div>
+      <h1>hello um a C-Master here.</h1>
+      <p>At moringa school 4IN5 group</p>
+      <Link to="/brian">Brian</Link>
+      
+      
+      <Brian/>
+     
     </div>
+  );
 }
 
-export default Brian
+export default Brian;
