33fc1d3113e3b8362cbe9789c069453d58823d13
brianbryank <brianbryan0125@gmail.com>
2023-08-25 17:50:24 +0300

    add app.js

--- collab/src/App.js
+++ collab/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import {Routes,Route, Link} from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Brian from './Brian';
 
 function App() {
   return (
     <div className="App">
-      <Brian/>
-      
+      <nav>
+        <button className="nav-link">
+          <Link to="/Robert">Robert</Link>
+        </button>
+      </nav>
+
+      <Routes>
+        <Route path="/Robert" element={<Robert />} />
+      </Routes>
     </div>
   );
 }
