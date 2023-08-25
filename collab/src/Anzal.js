7b6d137020fdee31f77f57b880dfbdb37520ccb3
+ <+>
2023-08-25 14:11:23 +0300

    Create Anzal.js component

---
+++ collab/src/Anzal.js
@@ -0,0 +1,9 @@
+
+function Anzal(){
+    return <div>
+        <h1>Hi, my name is Anzal Abdinoor</h1>
+        <p>Currently learning Software Engineering at Moringa School.</p>
+    </div>
+}
+
+export default Anzal
