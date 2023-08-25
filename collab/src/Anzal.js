c6e8a46b57066ad36385766886029b4a36fbfb38
+ <+>
2023-08-25 15:37:55 +0300

    create Anzal component

--- collab/src/Anzal.js
+++ collab/src/Anzal.js
@@ -1,8 +1,18 @@
-function Anzal(){
-    return <div>
-        <h1>Hi, my name is Anzal Abdinoor</h1>
-        <p>Currently learning Software Engineering at Moringa School.</p>
+import React from 'react';
+import Anzal from './Anzal';
+import { Link } from "react-router-dom"
+
+function Anzal() {
+  return (<div>
+      <h1>My name is Anzal Abdinoor.</h1>
+      <p>I am currently studying Software Engineering</p>
+      <Link to="/anzal">Anzal</Link>
+      
+      
+      <Anzal/>
+     
     </div>
+  );
 }
 
-export default Anzal
+export default Anzal;
