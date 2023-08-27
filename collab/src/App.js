8e4babc05a989df73754af77aa058c393ed4cd7c
robertnjunge <mubearobert@gmail.com>
2023-08-27 20:41:02 +0300

    add robert component

--- collab/src/App.js
+++ collab/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import {Routes,Route, Link} from 'react-router-dom';
-import './App.css';
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        <button className="nav-link">
+          <Link to="/Robert">Robert</Link>
+        </button>
+      </nav>
+
+      <Routes>
+        <Route path="/Robert" element={<Robert />} />
+      </Routes>
     </div>
   );
 }
--- collab/src/Robert.js
+++ collab/src/Robert.js
@@ -1,9 +1,13 @@
+import React from "react"
 import { Link } from "react-router-dom"
 
 function Robert(){
     return <div>
-        <h1>This is Robert Component</h1>
-    </div>
+      <h1>My name Robert Njung'e</h1>
+      <p>I love making new friends</p>
+      <p>I am a big fan of Expository and Real-life Documentaries</p>
+      <Link to="/Robert">Robert</Link>
+       </div>
 }
 
 export default Robert
